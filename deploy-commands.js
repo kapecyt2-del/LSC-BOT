a8bba9bedd9fe415ff630f82bed8f961dc2d3255
kapecyt2-del <kapecyt2@gmail.com>
2026-07-22 20:58:01 +0200

    Dodano rejestracje komend

---
+++ deploy-commands.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+
+const { REST, Routes } = require('discord.js');
+
+const commands = [];
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath)
+    .filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+
+    const command = require(path.join(commandsPath, file));
+
+    commands.push(command.data.toJSON());
+
+    console.log(`✅ Dodano komendę: ${command.data.name}`);
+}
+
+const rest = new REST({ version: '10' })
+    .setToken(process.env.TOKEN);
+
+
+(async () => {
+
+    try {
+
+        console.log('🔄 Rejestrowanie komend...');
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            {
+                body: commands
+            }
+        );
+
+        console.log('✅ Komendy zarejestrowane!');
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
+})();
